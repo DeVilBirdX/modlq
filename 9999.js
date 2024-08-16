609d955b27115b1f7b0ea7d3f044a35e9ccff663
DeVilBirdX <luannguyenthanh778@gmail.com>
2024-08-16 15:29:06 +0700

    Add files via upload

---
+++ 9999.js
@@ -0,0 +1,12 @@
+function unlockPage() {
+    const validKeys = ["LqDay1-Cx72is9", "LqDay1-Cx72is9"];
+    const inputKey = document.getElementById("key-input").value.replace(/\s+/g, ''); 
+    const errorMessage = document.getElementById("error-message");
+
+    if (validKeys.map(key => key.replace(/\s+/g, '')).includes(inputKey)) {
+        document.getElementById("lock-screen").style.display = "none";
+        document.getElementById("content").style.display = "block";
+    } else {
+        errorMessage.style.display = "block";
+    }
+}
