2cc6d9d420210e32822938fd95c2779ae214a79d
DeVilBirdX <luannguyenthanh778@gmail.com>
2024-08-16 20:07:07 +0700

    Add files via upload

--- 9999.js
+++ 9999.js
@@ -1,12 +1,12 @@
 function unlockPage() {
-    const validKeys = ["LqDay1-Cx72is9", "LqDay1-Cx72is9"];
+    const validKeys = ["LqDay1-Cx72is9", "Kunn-456798"];
     const inputKey = document.getElementById("key-input").value.replace(/\s+/g, ''); 
     const errorMessage = document.getElementById("error-message");
 
     if (validKeys.map(key => key.replace(/\s+/g, '')).includes(inputKey)) {
         document.getElementById("lock-screen").style.display = "none";
+        document.getElementById("youtube-container").style.display = "none";  
         document.getElementById("content").style.display = "block";
-        document.querySelector(".youtube-container").style.display = "none";
     } else {
         errorMessage.style.display = "block";
     }
