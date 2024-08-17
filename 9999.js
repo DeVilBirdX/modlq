a03cd8c88317270ad5b045d561736d00b79ce0c3
DeVilBirdX <luannguyenthanh778@gmail.com>
2024-08-17 16:29:13 +0700

    Add files via upload

--- 9999.js
+++ 9999.js
@@ -5,7 +5,8 @@ function unlockPage() {
 
     if (validKeys.map(key => key.replace(/\s+/g, '')).includes(inputKey)) {
         document.getElementById("lock-screen").style.display = "none";
-        document.getElementById("youtube-container").style.display = "none";  
+        document.getElementById("youtube-container").style.display = "none";
+        document.getElementById("copyright-text").style.display = "none";  
         document.getElementById("content").style.display = "block";
     } else {
         errorMessage.style.display = "block";
