0625e12c181cac22eed327483f23b594dff880ae
DeVilBirdX <luannguyenthanh778@gmail.com>
2024-08-30 17:34:26 +0700

    Add files via upload

--- 9999.js
+++ 9999.js
@@ -1,5 +1,5 @@
 function unlockPage() {
-    const validKeys = ["sapcapnhat", "tulenvp0235/"];
+    const validKeys = ["tháng9", "tulenvp0235/"];
     const inputKey = document.getElementById("key-input").value.replace(/\s+/g, ''); 
     const errorMessage = document.getElementById("error-message");
 
