a6786c38c6852b3717565a6118b72dc96fdb3545
DeVilBirdX <luannguyenthanh778@gmail.com>
2024-09-11 17:37:51 +0700

    Add files via upload

--- 9999.js
+++ 9999.js
@@ -1,5 +1,5 @@
 function unlockPage() {
-    const validKeys = ["v78hsp", "tulenvp0235/"];
+    const validKeys = ["hs929k", "tulenvp0235/"];
     const inputKey = document.getElementById("key-input").value.replace(/\s+/g, ''); 
     const errorMessage = document.getElementById("error-message");
 
