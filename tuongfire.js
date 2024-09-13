d53a3f23dc85e62277322ddc0773bea0ad3b4f1e
DeVilBirdX <luannguyenthanh778@gmail.com>
2024-09-14 04:34:26 +0700

    Add files via upload

--- tuongfire.js
+++ tuongfire.js
@@ -1,5 +1,12 @@
-const protectedPaths = ['/keysweb.json', '/index.html', '/8888.js', '/acc.json', '/manifest.json', '/tuongfire.js'];
+const protectedPaths = [
+  '/keysweb.json',
+  '/index.html',
+  '/8888.js',
+  '/acc.json',
+  '/manifest.json',
+  '/tuongfire.js'
+];
 
 if (protectedPaths.includes(window.location.pathname)) {
-    window.location.href = '/404.html';
+    window.location.href = 'https://modaov.netlify.app/404.html';
 }
