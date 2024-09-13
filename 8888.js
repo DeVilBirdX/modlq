bbcca0024cde3efa5ea2fd6b11d11b5f8c1ab678
DeVilBirdX <luannguyenthanh778@gmail.com>
2024-09-14 02:42:49 +0700

    Add files via upload

--- 8888.js
+++ 8888.js
@@ -1,22 +1,23 @@
-async function fetchValidKeys() {
-    const response = await fetch('validKeys.cbor'); 
-    const buffer = await response.arrayBuffer();    
-    return cbor.decode(buffer);                     
-}
-
-async function unlockPage() {
+function unlockPage() {
     const inputKey = document.getElementById("key-input").value.replace(/\s+/g, ''); 
     const errorMessage = document.getElementById("error-message");
 
-    const validKeys = await fetchValidKeys();       
-
-    if (validKeys.map(key => key.replace(/\s+/g, '')).includes(inputKey)) {
-        document.getElementById("lock-screen").style.display = "none";
-        document.getElementById("youtube-container").style.display = "none";
-        document.getElementById("copyright-text").style.display = "none";  
-        document.getElementById("chúcmừngnămmới").style.display = "none";  
-        document.getElementById("content").style.display = "block";
-    } else {
-        errorMessage.style.display = "block";
-    }
+    fetch('keysweb.json')
+        .then(response => response.json())
+        .then(data => {
+            const validKeys = data.keysweb.map(key => key.replace(/\s+/g, '')); 
+            if (validKeys.includes(inputKey)) {
+                document.getElementById("lock-screen").style.display = "none";
+                document.getElementById("youtube-container").style.display = "none";
+                document.getElementById("copyright-text").style.display = "none";  
+                document.getElementById("chúcmừngnămmới").style.display = "none";  
+                document.getElementById("content").style.display = "block";
+            } else {
+                errorMessage.style.display = "block";
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching keys:', error);
+            errorMessage.style.display = "block";
+        });
 }
