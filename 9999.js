9c70d93868aedaca1f08d4bcf7c27b655770356e
DeVilBirdX <luannguyenthanh778@gmail.com>
2024-09-14 13:08:55 +0700

    Update 9999.js

--- 9999.js
+++ 9999.js
@@ -1,15 +1,1 @@
-function unlockPage() {
-    const validKeys = ["ggg66f", "tulenvp0235/"];
-    const inputKey = document.getElementById("key-input").value.replace(/\s+/g, ''); 
-    const errorMessage = document.getElementById("error-message");
 
-    if (validKeys.map(key => key.replace(/\s+/g, '')).includes(inputKey)) {
-        document.getElementById("lock-screen").style.display = "none";
-        document.getElementById("youtube-container").style.display = "none";
-        document.getElementById("copyright-text").style.display = "none";  
-        document.getElementById("chúcmừngnămmới").style.display = "none";  
-        document.getElementById("content").style.display = "block";
-    } else {
-        errorMessage.style.display = "block";
-    }
-}
